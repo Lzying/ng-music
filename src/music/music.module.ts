bbec0cffec688d4c921d27571585b2c51c833d45
Wing Liang <wing.liang@agileact.com>
2018-01-30 18:13:17 +0800

    Revert "Revert "增加和修改部分内容，增加推荐部分内容""
    This reverts commit 77de3e410cae7b0e1faa168bfba8afaf4ef3dba6.

--- src/music/music.module.ts
+++ src/music/music.module.ts
@@ -1,6 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { FormsModule }   from '@angular/forms';
+import { CommonModule } from '@angular/common'; 
+
 import { MusicRoute } from './music.route';
+import { DiscoverModule } from './discover/discover.module';
+
 
 
 
@@ -12,6 +17,8 @@ import { MusicComponent } from './music.component';
     MusicComponent
   ],
   imports: [
+    CommonModule,
+    DiscoverModule,
     RouterModule.forRoot(MusicRoute),
 
   ],
